878878e84654d055449952556a183f7a1ba0c352
Andreas H. Koenig <ahkoenig17@googlemail.com>
2012-10-11 23:17:02 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlitzStatus::Application.config.secret_token = 'a4d06f358b6bef731fe0afd5807b78fbb5f49183246c90430b3b2e642ae5d0d8723f525bb99f5cc872b9efa783d7f4c23b82d0373685a1cd26875ef55844645c'
